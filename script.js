d9e1a8d0122f553660feb5e893cdc79d65dd4011
mohilali <mouad.fdm@gmail.com>
2021-12-30 02:20:17 +0100

    added dropping of fruits in the startAction function and game over details, also defined the stopAction function that stops dropping fruits.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var playing = false;
-var score, trialsLeft;
-var fruits = ["apple", "banana","cherries","grapes","mango","orange","watermelon","strawberry","pear"];
+var score, trialsLeft, step;
+var action; //used for the set interval function
+var fruits = ["apple", "banana", "cherries", "grapes", "mango", "orange", "watermelon", "strawberry", "pear"];
 
-$(function(){
+$(function() {
      //click on start reset button
-     $("#startreset").click(function(){
+     $("#startreset").click(function() {
           //we are playing
-          if (playing){
-
+          if (playing) {
                //reload page
                location.reload();
           } else {
-
                //we are not playing
                playing = true; //game initiated
 
@@ -21,8 +20,11 @@ $(function(){
 
                //show trials left
                $("#trialsLeft").show();
-               trialsLeft=3;
+               trialsLeft = 3;
                addHearts();
+
+               //
+               $("#gameover").hide();
 
                //change button text to reset game
                $("#startreset").html("Reset Game");
@@ -33,39 +35,93 @@ $(function(){
      });
 
 
-//functions
+     //functions
 
-//add hearts in the life bar
-function addHearts(){
-     for(i=0; i<trialsLeft; i++){
-          $("#trialsLeft").append('<img src="images/heart.png" class="life">');
+     //add hearts in the life bar
+     function addHearts() {
+          $("#trialsLeft").empty();
+          for (i = 0; i < trialsLeft; i++) {
+               $("#trialsLeft").append('<img src="images/heart.png" class="life">');
+          }
      }
-}
 
-//start sending fruits
-function startAction(){
-     $("#fruit1").show();
-     chooseFruit(); //choose a random fruit
+     //start sending fruits
+     function startAction() {
+          //generate a fruit
+          $("#fruit1").show();
+          chooseFruit(); //choose a random fruit
 
-     $("#fruit1").css({'left' : Math.round(Math.random()*550), 'top':-50});
+          //random horizontal position
+          $("#fruit1").css({
+               'left': Math.round(Math.random() * 550),
+               'top': -50
+          });
 
-}
+          //generate a random step
+          step = 1 + Math.round(Math.random() * 5);
 
-//generate a random fruit
-function chooseFruit(){
-     $("#fruit1").attr('src', "images/" + fruits[Math.round(Math.random()*8)] + ".png");
-}
+          //move fruit down by one step every 10 ms
+          action = setInterval(function() {
+               //move fruit by one step
+               $("#fruit1").css('top', $("#fruit1").position().top + step);
+
+               //check if the fruit is too low
+               if ($("#fruit1").position().top > $("#fruitsContainer").height()) {
+                    //check if we have trials left
+                    if (trialsLeft > 1) {
+                         //generate a fruit
+                         $("#fruit1").show();
+                         chooseFruit(); //choose a random fruit
+
+                         //random horizontal position
+                         $("#fruit1").css({
+                              'left': Math.round(Math.random() * 550),
+                              'top': -50
+                         });
+
+                         //generate a random step
+                         step = 1 + Math.round(Math.random() * 5);
+
+                         //reduce trials by one
+                         trialsLeft--;
+
+                         //populate the life bar with the new number of hears
+                         addHearts();
+                    } else {
+                         //Game Over
+                         playing = false; //we are not playing anymore
+                         $("#startreset").html("Start Game"); //change button ro Start Game
+                         $("#gameover").show();
+                         $("#trialsLeft").hide();
+                         $("#gameover").html('<p>Game Over!</p> <p>Your score is ' + score + '</p>');
+                         stopAction();
+                    }
+               }
+          }, 10);
+
+     }
+
+     //generate a random fruit
+     function chooseFruit() {
+          $("#fruit1").attr('src', "images/" + fruits[Math.round(Math.random() * 8)] + ".png");
+     }
+
+     //stop dropping fruits
+     function stopAction(){
+          clearInterval(action);
+          $("#fruit1").hide();
+     }
 
 });
 
 
 //click on start reset button
 
-          //yes
-               //reload page
-          //no
-               //show trials left
-               //change button text to "reset game"
-               //1. create a random fruint
-               //define a random stem
-               //2.move fruite down one step
+//yes
+//reload page
+//no
+//show trials left
+//change button text to "reset game"
+//1. create a random fruint
+//define a random stem
+//2.move fruite down one step
